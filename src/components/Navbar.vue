c6d7dcb28618f0af11b52c91459b66c2824e6ef5
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-05-13 16:32:21 -0300

    Commit inicial
    começando projeto de site para empresa

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main-navbar">
+    <router-link class="inicio" to="/">Início</router-link> 
+    <router-link class="fale" to="/about">Fale Conosco</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+.main-navbar {
+  padding: 20px;
+  height: 80px;
+  background-color: aqua;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  
+
+}
+
+.inicio,
+.fale {
+  margin-right: 40px;
+  text-decoration: none;
+  font-size: 2rem;
+}
+
+</style>
